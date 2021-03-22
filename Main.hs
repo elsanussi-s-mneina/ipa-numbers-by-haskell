fb2eaa792c303be05ec5f12fd05f79cfa2d0a000
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2021-03-22 17:08:59 -0500

    more documentation

--- Main.hs
+++ Main.hs
@@ -5,21 +5,31 @@ import System.Environment (getArgs)
 import IPANumberToUnicode 
 import UnicodeToIPANumber
 
+-- | The version number
 versionNumber :: String
 versionNumber = "0.3.0"
 
+-- | The user-facing name of the program
 programName :: String
 programName = "IPA Number and Character Converter"
 
+-- | This is a short message to show users who need to know how to use it.
 helpMessage :: String
 helpMessage = "Usage: \n --cn \t To convert a character to a number use the flag.\n --nc \t To convert a number to a character use the flag. \n --version \t To see the version number of this executable."
 
+-- | A function to convert an IPA character to a number
+-- when both are stored as strings.
 ipaCharacterToNumberWrapper :: String -> String
 ipaCharacterToNumberWrapper = show . unicodeToNumber . head
 
+-- | a function to convert IPA numbers to characters
+-- when both are stored as strings.
 numberToUnicodeWrapper :: String -> String
 numberToUnicodeWrapper = return . numberToUnicode . read 
 
+-- | Prints a message explaining how to use the program.
+-- It prints the information to the command line for 
+-- the user of the program to read.
 showHelp :: IO ()
 showHelp = putStrLn helpMessage
 
@@ -38,6 +48,8 @@ showProgramVersion = putStrLn (unwords [programName, versionNumber])
 perWord :: (String -> String) -> (String -> String)
 perWord x = unwords . map x . words
 
+-- | The function decides what
+-- to do based on command line arguments.
 handleArguments :: [String] -> IO ()
 handleArguments args =
   case args of 
@@ -47,7 +59,9 @@ handleArguments args =
     _ -> showHelp
 
 
-
+-- | Program execution starts in the `main` function.
+-- It gets the command line arguments
+-- in order to handle them.
 main :: IO ()
 main =
   getArgs >>= handleArguments 
